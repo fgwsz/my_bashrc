312d18daf20ca2713243fb89a9af3702f392b248
fgwsz <3191000458@qq.com>
2024-05-25 17:55:54 +0800

    add git pull all

--- my_bashrc.sh
+++ my_bashrc.sh
@@ -47,6 +47,18 @@ alias vimrc_git_to_sys='~/Gitpro/my_vimrc/vimrc_git_to_sys.sh'
 alias vimrc_sys_to_git='~/Gitpro/my_vimrc/vimrc_sys_to_git.sh'
 alias bashrc_git_to_sys='~/Gitpro/my_bashrc/bashrc_git_to_sys.sh'
 alias bashrc_sys_to_git='~/Gitpro/my_bashrc/bashrc_sys_to_git.sh'
+function git_pull_all {
+    for dir in */; do
+        if [ -d "$dir/.git" ]; then
+            echo "Pulling changes in $dir"
+            cd "$dir"
+            git pull
+            cd ..
+        else
+            echo "$dir does not contain a .git directory, skipping."
+        fi
+    done
+}
 #=============================================================================
 #用户自定义结束
 #=============================================================================
