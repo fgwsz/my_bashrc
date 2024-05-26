6ceea3950f70a54d1d7c677f4f0d0f9b2f7192dc
fgwsz <3191000458@qq.com>
2024-05-26 09:15:10 +0800

    clean

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-git add ./*.sh
+git add ./my_bashrc.sh
+git add ./bashrc_git_to_sys.sh
+git add ./bashrc_sys_to_git.sh
+git add ./git_push.sh
 read -p "Input Git Commit Info: " commit_info
 git commit -m "$commit_info"
 git push
